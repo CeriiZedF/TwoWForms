49b77f192f372e6334001df97cff4f21eaa363b4
CeriiZedF <dasbalan14@gmail.com>
2023-06-10 21:11:52 +0300

    -

---
+++ Child.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoWForms
+{
+    public partial class Child : Form
+    {
+        private Parent parent;
+        public Child(Parent parent)
+        {
+            InitializeComponent();
+            this.parent = parent;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var temp = sender as TextBox;
+            parent.UpdateForm(ref temp);
+        }
+        public void UpdateForm(ref TextBox temp)
+        {
+            textBox1.Text = temp.Text;
+        }
+    }
+}
---
+++ Parent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoWForms
+{
+    public partial class Parent : Form
+    {
+        Child child;
+        public Parent()
+        {
+            InitializeComponent();
+            child = new Child(this);
+            child.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var temp = sender as TextBox;
+            child.UpdateForm(ref temp);
+        }
+
+        public void UpdateForm(ref TextBox temp)
+        {
+            textBox1.Text = temp.Text;
+        }
+
+        private void Parent_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
